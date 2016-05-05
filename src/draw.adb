c27c5d36587d8b007522c530528b39a02e59d787
Johan <johaa1993@gmail.com>
2016-05-05 22:30:43 +0200

    Misc

--- src/draw.adb
+++ src/draw.adb
@@ -8,12 +8,15 @@ with GL.Shaders.Files;
 with GL.Uniforms;
 with GL.Vertex_Attributes;
 with GL.Buffers;
+with GL.Drawings;
 
 with GLFW3;
 with GLFW3.Windows;
 with GLFW3.Windows.Keys;
 
 with Ada.Text_IO;
+
+with System;
 
 with OpenGL_Loader_Test;
 
@@ -69,12 +72,14 @@ procedure Draw is
       use Ada.Text_IO;
       use Cameras;
       use GL.Uniforms;
+      use GL.Buffers;
+      use GL.Drawings;
    begin
       loop
          Poll_Events;
 
-         delay 0.1;
-         Clear_Screen;
+         Clear (Color_Plane);
+         Clear (Depth_Plane);
 
          if Get_Key (W, Key_Up) = Key_Action_Press then
             Put_Line ("Key_Up");
@@ -108,37 +113,88 @@ procedure Draw is
 
          Modify (L, Build (C)'Address);
 
+
+--           delay 0.1;
+--           Clear_Screen;
+
+         Draw (Triangle_Mode, 0, 32);
+
+         Swap_Buffers (W);
+
+         pragma Warnings (Off);
+         exit when Window_Should_Close (W) = 1;
+         pragma Warnings (On);
+      end loop;
+   end;
+   pragma Unreferenced (Render_Loop);
+
+
+   procedure Render_Loop (W : GLFW3.Window) is
+      use GLFW3;
+      use GLFW3.Windows;
+      use GLFW3.Windows.Keys;
+      use OS_Systems;
+      use Ada.Text_IO;
+      use Cameras;
+      use GL.Uniforms;
+      use GL.Buffers;
+      use GL.Drawings;
+   begin
+      loop
+         Poll_Events;
+         Clear (Color_Plane);
+         --Clear (Depth_Plane);
+         Draw (Triangle_Mode, 0, 3);
+         Swap_Buffers (W);
          pragma Warnings (Off);
          exit when Window_Should_Close (W) = 1;
          pragma Warnings (On);
       end loop;
    end;
 
+
    function Setup_Window return GLFW3.Window is
       use GLFW3;
       use GLFW3.Windows;
       use GL.C.Initializations;
+      use GL.Drawings;
       W : constant Window := Create_Window_Ada (400, 400, "Hello");
    begin
+      Viewport (0, 0, 400, 400);
       Make_Context_Current (W);
       Initialize (OpenGL_Loader_Test'Unrestricted_Access);
       return W;
    end;
 
 
-   procedure Setup_Vertices (Item : GL.Buffers.Buffer) is
+   function Setup_Vertices (Vert_Attr : GL.Vertex_Attributes.Location) return GL.Buffers.Buffer is
       use GL.Vertex_Attributes;
       use GL.Buffers;
+
       type Vector is array (Integer range <>) of Float;
+      for Vector'Component_Size use 32;
+
       subtype Vector_3 is Vector (1 .. 3);
       subtype Vector_4 is Vector (1 .. 4);
+
+
       type Vertex is record
          A : Vector_3;
          C : Vector_4;
       end record;
+
       type Vertex_Array is array (Integer range <>) of Vertex;
+      for Vertex'Alignment use 32;
+      for Vertex_Array'Alignment use 32;
+
       Cube : Vertex_Array (1 .. 36);
+      Buf_Obj : constant Buffer := Generate;
+      VBO : constant Config := Generate;
    begin
+      Bind (VBO);
+      Bind (Buf_Obj, Array_Slot);
+      Allocate (Array_Slot, Cube'Size / System.Storage_Unit, Static_Usage);
+
       Cube (1).A := (-1.0,-1.0,-1.0);
       Cube (2).A := (-1.0,-1.0, 1.0);
       Cube (3).A := (-1.0, 1.0, 1.0);
@@ -175,7 +231,12 @@ procedure Draw is
       Cube (34).A := (1.0, 1.0, 1.0);
       Cube (35).A := (-1.0, 1.0, 1.0);
       Cube (36).A := (1.0,-1.0, 1.0);
+      Redefine (Array_Slot, 0, Cube'Size / System.Storage_Unit, Cube'Address);
 
+      Enable_Vertex_Attribute_Array (Vert_Attr);
+      Set_Vertex_Attribute (Vert_Attr, Vector_3'Size / System.Storage_Unit, Float_Type, False, Vertex_Array'Component_Size / System.Storage_Unit, 0);
+
+      return Buf_Obj;
    end;
 
 
@@ -192,19 +253,24 @@ begin
       use GL.Uniforms;
       use GL.Vertex_Attributes;
       use GL.Programs.Vertex_Attributes;
+      use GL.Buffers;
       use Cameras;
       W : constant Window := Setup_Window;
       P : constant Program := Setup_Program;
-      UL : constant GL.Uniforms.Location := Get (P, "transform");
+      --UL : constant GL.Uniforms.Location := Get (P, "transform");
       VL : constant GL.Vertex_Attributes.Location := Get (P, "position");
       C : Camera := Create_RC;
       CC : Character;
+      B : constant Buffer := Setup_Vertices (VL);
    begin
-      Put_Line_Fancy (UL);
+      --Put_Line_Fancy (UL);
       Put_Line_Fancy (VL);
       Get_Immediate (CC);
       Perspective_RC (C, 90.0, 3.0/4.0, 5.0, 80.0);
-      Render_Loop (W, UL, C);
+      Bind (B, Array_Slot);
+      Set_Current (P);
+      --Render_Loop (W, UL, C);
+      Render_Loop (W);
       Destroy_Window (W);
    end;
 
